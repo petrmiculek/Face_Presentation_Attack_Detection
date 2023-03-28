fb75bb1cee189b8c57f93ed4af674cf813ee570f
petrmiculek <petr.miculek@gmail.com>
2023-03-28 23:13:51 +0200

    warn_limit.sh script to be run before commit

---
+++ scripts/warn_limit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# go through all .py files line by line, and warn if a line contains "limit = \d+"
+
+# get all .py files
+files=`find . -name "*.py"`
+
+re="limit = [0-9]+"
+retval=0
+# go through all files
+for file in $files
+do
+    # go through all lines
+    while read line
+    do
+        # check if line contains "limit = \d+"
+        if [[ $line =~ $re ]]
+        then
+            echo "WARNING: $file contains a limit: $line"
+            retval=1
+        fi
+    done < "$file"
+done
+
+exit $retval
